ed0bb4fb35d8eef0318cf1f1f113f61ed3f5d255
skaznik <skaznik@wszib.edu.pl>
2020-01-26 11:46:36 +0100

    Zjazd2_spring

--- src/main/java/spring2/spring2/Controller/PierwszyKontroler.java
+++ src/main/java/spring2/spring2/Controller/PierwszyKontroler.java
@@ -1,9 +1,12 @@
 package spring2.spring2.Controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Calendar;
-
+@Validated
 @RequestMapping("/test")
 @RestController
 public class PierwszyKontroler {
@@ -17,7 +20,7 @@ public class PierwszyKontroler {
     @GetMapping("/dzienTygodnia/{rok}/{miesiac}/{dzien}")
     public int dzienTygodnia(
             @PathVariable int dzien,
-            @PathVariable int miesiac,
+            @PathVariable @Min(1) @Max(12) int miesiac,
             @PathVariable int rok){
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR, rok);
