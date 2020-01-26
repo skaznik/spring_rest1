c29b97612f4dd1bfa05229b896c53928a722261d
skaznik <skaznik@wszib.edu.pl>
2020-01-26 11:15:18 +0100

    Zjazd2_spring

--- src/main/java/spring2/spring2/Controller/PierwszyKontroler.java
+++ src/main/java/spring2/spring2/Controller/PierwszyKontroler.java
@@ -1,18 +1,23 @@
 package spring2.spring2.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/test")
 @RestController
 public class PierwszyKontroler {
 
-    @RequestMapping
+    @GetMapping("/dodaj/{a}/{b}")
+
+    public int dodaj(@PathVariable int a, @PathVariable int b) {
+        return a + b;
+    }
+
+
+    @GetMapping
     public String helloworld() {
         return "Hello world";
     }
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public String helloworld2() {
         return "Hello world2";
     }
