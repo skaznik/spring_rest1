1fa0e9ff222a5a9529ae9d3f73fbc3d0dc777b12
skaznik <skaznik@wszib.edu.pl>
2020-01-26 15:29:14 +0100

    Zjazd2_spring

--- src/main/java/spring2/spring2/Controller/PiatyKontroler.java
+++ src/main/java/spring2/spring2/Controller/PiatyKontroler.java
@@ -1,22 +1,30 @@
 package spring2.spring2.Controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.webjars.NotFoundException;
 import spring2.spring2.Model.User;
 
 @RequestMapping("/mieszany")
 @RestController
 public class PiatyKontroler {
+private User user;
 
     @PostMapping
     public User stworz(@RequestParam String name,
                        @RequestParam String nazwisko,
                        @RequestParam int wiek) {
-
-
+        user = new User(name, nazwisko, wiek);
+                return user;
     }
+     @PatchMapping("dodaj/uprawnienie")
+    public User dodaj(@PathVariable String uprawnienie) {
+        if (user == null) {
+            throw new NotFoundException("User not found");
+        }
+        user.addUprawnienia(uprawnienie);
+        return user
+
+     }
 
 }
