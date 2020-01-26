7150dd3fb2b9f69de0253accdec650079b5162f1
skaznik <skaznik@wszib.edu.pl>
2020-01-26 14:59:48 +0100

    Zjazd2_spring

--- src/main/java/spring2/spring2/Model/User.java
+++ src/main/java/spring2/spring2/Model/User.java
@@ -1,10 +1,30 @@
 package spring2.spring2.Model;
 
+import java.util.List;
+
 public class User {
   //do użytku w czwartym kontrolerze
     private String name;
     private String nazwisko;
     private int wiek;
+    private List<String> uprawnienia;
+    private User zona;
+
+    public List<String> getUprawnienia() {
+        return uprawnienia;
+    }
+
+    public void setUprawnienia(List<String> uprawnienia) {
+        this.uprawnienia = uprawnienia;
+    }
+
+    public User getZona() {
+        return zona;
+    }
+
+    public void setZona(User zona) {
+        this.zona = zona;
+    }
 
     public String getName() {
         return name;
